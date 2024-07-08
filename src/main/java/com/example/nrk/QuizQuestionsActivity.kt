b218f34709d5b64885da39e52af1668c13b47eee
Mourish Kanna S <mourishkumar1201@gmail.com>
2024-07-08 12:00:08 +0530

    Modified activity_quiz_questions.xml and activity so that it displays the questions from Constant file and also shows the progress

--- src/main/java/com/example/nrk/QuizQuestionsActivity.kt
+++ src/main/java/com/example/nrk/QuizQuestionsActivity.kt
@@ -2,38 +2,53 @@ package com.example.nrk
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.nrk.ui.theme.NRKTheme
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class QuizQuestionsActivity : ComponentActivity() {
+class QuizQuestionsActivity : AppCompatActivity() {
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var tvProgress: TextView
+    private lateinit var tvQuestion: TextView
+    private lateinit var ivImage: ImageView
+    private lateinit var tvOptionOne: TextView
+    private lateinit var tvOptionTwo: TextView
+    private lateinit var tvOptionThree: TextView
+    private lateinit var tvOptionFour: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        progressBar = findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tv_progress)
+        tvQuestion = findViewById(R.id.tv_question)
+        ivImage = findViewById(R.id.iv_image)
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
+
         val questionsList = Constants.getQuestions()
         Log.e("Questions Size", "${questionsList.size}")
         for (i in questionsList) {
             Log.e("Questions", i.question)
         }
-    }
-}
 
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition - 1]
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NRKTheme {
-        Greeting("Android")
+        progressBar.progress = currentPosition
+        tvProgress.text = "$currentPosition/${progressBar.max}"
+        tvQuestion.text = question?.question
+        question?.let {
+            ivImage.setImageResource(it.image)
+            tvOptionOne.text = it.optionOne
+            tvOptionTwo.text = it.optionTwo
+            tvOptionThree.text = it.optionThree
+            tvOptionFour.text = it.optionFour
+        }
     }
 }
