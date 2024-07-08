730a7d7b4e576742087f8ba47d11729d68b93e68
Mourish Kanna S <mourishkumar1201@gmail.com>
2024-07-08 11:08:17 +0530

    Display prompt if Name not entered

--- src/main/java/com/example/nrk/MainActivity.kt
+++ src/main/java/com/example/nrk/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.nrk
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,5 +22,22 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        val et_Name = findViewById<EditText>(R.id.et_Name)
+        val btn_start = findViewById<Button>(R.id.btn_start)
+
+        btn_start.setOnClickListener {
+
+            if (et_Name.text.toString().isEmpty()) {
+
+                Toast.makeText(this@MainActivity, "Please enter your name", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+
+                val intent = Intent(this@MainActivity, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
